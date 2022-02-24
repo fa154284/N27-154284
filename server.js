fa479f794f864599a414dfbc90b0667c0c8a4a35
fa154284 <fa154284@students.berufskolleg-borken.de>
2022-02-24 07:59:42 +0100

    ...

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-//Programme verarbeiten oft Objekte der realen Welt.
-//Objekte haben Eigenschaften
-//In unserem Bankingprogramm interessieren und Objekte
-//wie z.B. Kunde, Konto, Filiale, Bankautomat, ...
-//Alle Kunden unserer Bank haben dieselben Eigenschaften, aber
-//unterschiedliche Eigenschaftswerte
+// Programme verarbeiten oft Objekte der realen Welt.
+// Objekte haben Eigenschaften.
+// In unserem Bankingprogramm interessieren uns Objekte,
+// wie z.B. Kunde, Konto, Filiale, Bankautomat, ...
+// Alle Kunden unserer Bank haben dieselben Eigenschaften, aber
+// unterschiedliche Eigenschaftswerte
 
-class Kunde {
+class Kunde{
     constructor(){
         this.IdKunde
         this.Nachname
@@ -17,24 +17,24 @@ class Kunde {
     }
 }
 
-//Von der Kunden-Klasse wird eine konkrete Instanz
-//gebildet.
+// Von der Kunden-Klasse wird eine konkrte Instanz
+// gebildet. 
 
 let kunde = new Kunde()
 
-//Die konkrete Instanz bekommt EIgenschaftswerte
-//zugewiesen
+// Die konkrete Instanz bekommt Eigenschaftswerte
+// zugewiesen
 
 kunde.IdKunde = 150000
 kunde.Nachname = "Müller"
-kunde.Vorname = "Steven"
-kunde.Geburtsdatum = "23.10.2002"
+kunde.Vorname = "Pit"
+kunde.Geburtsdatum = "23.10.2000"
 kunde.Mail = "mueller@web.de"
 kunde.Kennwort = "123"
 
+
 const express = require('express')
 const bodyParser = require('body-parser')
-const req = require('express/lib/request')
 const meineApp = express()
 meineApp.set('view engine', 'ejs')
 meineApp.use(express.static('public'))
@@ -44,52 +44,77 @@ const server = meineApp.listen(process.env.PORT || 3000, () => {
     console.log('Server lauscht auf Port %s', server.address().port)    
 })
 
+// Die Methode meineApp.get('/' ...) wird abgearbeitet, wenn
+// der Kunde die Indexseite ansurft.
+
 meineApp.get('/',(browserAnfrage, serverAntwort, next) => {              
-    serverAntwort.render('index.ejs', {})          
+    
+    if(true){
+
+        // Wenn der Kunde bereits angemeldet ist, soll die
+        // Index-Seite an den Browser gegeben werden.
+
+        serverAntwort.render('index.ejs',{})
+    }else{
+
+        // Wenn der Kunde noch nicht eigeloggt ist, soll
+        // die Loginseite an den Browser zurückgegeben werden.
+        serverAntwort.render('login.ejs', {
+            meldung : ""
+        })
+    }                 
 })
 
-meineApp.post('/login',(browserAnfrage, serverAntwort, next) => { 
+meineApp.post('/login',(browserAnfrage, serverAntwort, next) => {              
     
-    const idKunde = browserAnfrage.body.idKunde
-    const kennwort = browserAnfrage.body.kennwort
+    const idKunde = browserAnfrage.body.IdKunde
+    const kennwort = browserAnfrage.body.Kennwort
+    
+    console.log("ID des Kunden: " + idKunde)
+    console.log("Kennwort des Kunden: " + kennwort)
 
-    console.log("ID des Kunden: "+ idKunde)
-    console.log("Kennwort des Kunden: "+ kennwort)
-
-// Die Indentität des Kunden wird überprüft
-
-    if(idKunde == kunde.IdKunde && kennwort == kunde.kennwort){
-
-        // Wenn die Id des KUnden mit der Eingabe im Browser übereinstimmt
+    // Die Identität des Kunden wird überprüft.
+    
+    if(idKunde == kunde.IdKunde && kennwort == kunde.Kennwort){
+    
+        // Wenn die Id des Kunden mit der Eingabe im Browser übereinstimmt
         // UND ("&&") das Kennwort ebenfalls übereinstimmt,
-        // gerendert gibt der Server die gerenderte Index-Seite zurück.
-
+        // dann gibt der Server die gerenderte Index-Seite zurück.
+        
         serverAntwort.render('index.ejs', {})
     }else{
 
-        // Wenn entweder die eingegebende Id oder das Kennwort oder beides
-        // nicht übereinstimmt, wird der Login verwigert. Es wird dann die
-        // gerenderte LOgin-Seite an den Browser zurückgegeben.
-        serverAntwort.render('login.ejs', {})  
-    }     
+        // Wenn entweder die eingegebene Id oder das Kennwort oder beides
+        // nicht übereinstimmt, wird der Login verweigert. Es wird dann die
+        // gerenderte Login-Seite an den Browser zurückgegeben.
+
+        serverAntwort.render('login.ejs', {
+            meldung : "Ihre Zugangsdaten scheinen nicht zu stimmen."
+        })
+    }
 })
 
 
-// Wenn die login-Seite im Browser aufgerufen wird,...
+// Wenn die login-Seite im Browser aufgerufen wird, ...
 
-meineApp.get('/login',(browserAnfrage, serverAntwort, next)=> {
-    
-  //...dann wird die login.ejs vom Server gerendert an den
-  //Browser zurückgegeben  
-    
-    serverAntwort.render('login.ejs', {})
+meineApp.get('/login',(browserAnfrage, serverAntwort, next) => {              
+
+    // ... dann wird die login.ejs vom Server gerendert an den
+    // Browser zurückgegeben:
+
+    serverAntwort.render('login.ejs', {
+        meldung : "Bitte geben Sie die Zugangsdaten ein."
+    })          
 })
 
-//Die meineApp.post('login') wird ausgeführt, sobald der Button 
-//auf dem Login-Formular gedrückt wird.
+// Die meineApp.post('login') wird ausgeführt, sobald der Button
+// auf dem Login-Formular gedrückt wird.
 
 meineApp.post('/login',(browserAnfrage, serverAntwort, next) => {              
-    serverAntwort.render('index.ejs', {})
-})  
+    serverAntwort.render('index.ejs', {})          
+})
+
+// require('./Uebungen/ifUndElse.js')
+require('./Uebungen/klasseUndObjekt.js')
 
 
